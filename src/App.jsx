777aa686395f719390454350be5de32a09a65f6f
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-12-15 16:24:40 +0300

    Adding simple todo logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,77 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
+
+  const toggleTodoCompleted = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== todoId) return todo;
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <label>
+        <input value={text} onChange={(event) => setText(event.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </label>
+
+      <ul style={{ listStyle: "none" }}>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodoCompleted(todo.id)}
+            />
+            <span>{todo.text}</span>
+            <button
+              style={{ color: "red", margin: "5px", cursor: "pointer" }}
+              on
+              onClick={() => removeTodo(todo.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
